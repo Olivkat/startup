2ddc36b2633e99567c56f1219ce0093d62fba8db
Olivkat <144805957+Olivkat@users.noreply.github.com>
2023-10-01 15:24:07 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+                    btn1.addEventListener("click", function () {
+                        if (a2.checked) {
+                            alert("Питання 1: Це правильна відповідь!");
+                        } else {
+                            alert("Питання 1: Подумайте ще раз!");
+                        }
+                    });
+
+                    btn2.addEventListener("click", function () {
+                        if (b1.checked) {
+                            alert("Питання 2: Це правильна відповідь!");
+                        } else {
+                            alert("Питання 2: Подумайте ще раз!");
+                        }
+                    });
+btn3.addEventListener("click", function () {
+                        if (c4.checked) {
+                            alert("Питання 3: Це правильна відповідь!");
+                        } else {
+                            alert("Питання 3: Подумайте ще раз!");
+                        }
+});
+                    btn4.addEventListener("click", function () {
+                        if (d1.checked) {
+                            alert("Питання 3: Це правильна відповідь!");
+                        } else {
+                            alert("Питання 3: Подумайте ще раз!");
+                        }
+                    });
+                    btn5.addEventListener("click", function () {
+                        if (e5.checked) {
+                            alert("Питання 3: Це правильна відповідь!");
+                        } else {
+                            alert("Питання 3: Подумайте ще раз!");
+                        }
+                    });
